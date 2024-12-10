744cd463bab3bf1888f8ea9b25c40310a1587fb3
Taxupka <167883032+Taxupka@users.noreply.github.com>
2024-12-10 11:18:50 +0300

    Add files via upload

---
+++ 7_2.py
@@ -0,0 +1,24 @@
+
+def custom_write(file_name, strings):
+    strings_positions = {}
+    file = open(file_name, 'w', encoding='utf-8')
+    for ind, strng in enumerate(strings, start=1):
+        b = file.tell()
+        file.write(f'{strng}\n')
+        tuple1 = (ind, b)
+        strings_positions[tuple1] = strng
+    file.close()
+
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
